2cbdd897f79394170f2b8a571ea19a696279343e
Felipe Burgos <burgosfelipeaa@gmail.com>
2017-11-11 14:02:12 -0300

    Primera versión

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,6 +5,7 @@ import { Http } from '@angular/http';
 export class ProductosService {
 
   productos: any[] = [];
+  productos_filtrado: any[] = [];
   cargando: boolean = true;
 
   constructor(private http: Http) {
@@ -15,20 +16,56 @@ export class ProductosService {
 
     this.cargando = true;
 
-      this.http.get("https://paginaweb-fe4e4.firebaseio.com/productos_idx.json")
+      let promesa = new Promise(( resolve, reject )=>{
+
+        this.http.get("https://paginaweb-fe4e4.firebaseio.com/productos_idx.json")
         .subscribe( res => {
 
           // console.log(res.json());
           // setTimeout(() => {
-            this.productos = res.json();
-            this.cargando = false;
+          this.productos = res.json();
+          this.cargando = false;
+          resolve();
           // }, 1500);
 
-        })
+        });
+      });
+      return promesa;
 
   }
 
   public cargar_producto(cod: string){
     return this.http.get(`https://paginaweb-fe4e4.firebaseio.com/productos/${ cod }.json`);
   }
+
+  public buscar_producto(termino: string){
+
+    // console.log('Buscando producto');
+    // console.log(this.productos.length);
+
+    if( this.productos.length === 0 ){
+      this.cargar_productos().then(()=>{
+        //termino la carga
+        this.filtrar_productos(termino);
+      });
+    }else{
+      this.filtrar_productos(termino);
+    }
+  }
+
+  private filtrar_productos(termino: string){
+
+    this.productos_filtrado = [];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach(prod => {
+      if(prod.categoria.indexOf(termino) >=0 || prod.titulo.toLowerCase().indexOf(termino) >=0){
+        this.productos_filtrado.push( prod );
+              // console.log(prod);
+      }
+
+    });
+  }
+
 }
